99d83704e7720af8552d531b968471ab3d231b61
Ethan <luo123qiu@gmail.com>
2017-07-21 12:19:55 +0800

    完善框架

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import CarList from '@/view/CarList'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'CarList',
+            component: CarList
+        }
+    ]
 })
---
+++ src/view/CarList.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <div>我擦</div>
+        <a href="#/">返回</a>
+    </div>
+</template>
